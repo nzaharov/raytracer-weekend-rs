70450b63445d295c34d502f42b5a5955034290af
Nikolay Zaharov <nzaharov@x570.localdomain>
2020-12-22 22:29:50 +0200

    hitlist

--- src/hit.rs
+++ src/hit.rs
@@ -28,3 +28,41 @@ impl Hit {
 pub trait Hittable {
     fn hit(&self, ray: &Ray, t_min: f32, t_max: f32) -> Option<Hit>;
 }
+
+pub struct HitList<'a, T>(Vec<&'a T>);
+
+impl<'a, T> HitList<'a, T>
+where
+    T: Hittable,
+{
+    pub fn new() -> Self {
+        Self(vec![])
+    }
+
+    pub fn add(&mut self, obj: &'a T) {
+        self.0.push(obj);
+    }
+
+    pub fn clear(&mut self) {
+        self.0.clear();
+    }
+}
+
+impl<'a, T> Hittable for HitList<'a, T>
+where
+    T: Hittable,
+{
+    fn hit(&self, ray: &Ray, t_min: f32, t_max: f32) -> Option<Hit> {
+        let mut closest_hit_t = t_max;
+        let mut current_hit: Option<Hit> = None;
+
+        for &obj in self.0.iter() {
+            if let Some(hit) = obj.hit(ray, t_min, closest_hit_t) {
+                closest_hit_t = hit.t;
+                current_hit = Some(hit);
+            }
+        }
+
+        current_hit
+    }
+}
